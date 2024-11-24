2ebdb0c1d6f089121722fc0a014a88d6ca126ac1
LukeTheWalker <lucag2001@gmail.com>
2024-11-24 01:21:35 +0100

    better separation of code

---
+++ impls/sequential_impl.hpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstring>
+
+struct pii {
+    pii * head;
+    int value;
+};
+
+void print_node(pii * node, size_t n){
+    for (size_t i = 0; i < n; i++){
+        std::cout << node->value << " ";
+        node = node->head;
+    }
+    std::cout << std::endl;
+}
+
+size_t pne_seq(int ** C, int * u, int n){
+    size_t * level_sizes = new size_t[n];
+    level_sizes[0] = u[0];
+    for (size_t i = 1; i < n; i++) level_sizes[i] = u[i] * level_sizes[i-1];
+    
+    // print level sizes
+    // for (size_t i = 0; i < n; i++) cout << "Level " << i << " size = " << level_sizes[i] << endl;
+
+    size_t * active_nodes_in_level = new size_t[n];
+    memset(active_nodes_in_level, 0, n*sizeof(size_t));
+
+    // vector<vector<vector<int> > > levels(n);
+    pii ** levels = new pii*[n];
+    for (size_t i = 0; i < n; i++){
+        levels[i] = new pii[level_sizes[i]];
+    }
+
+    active_nodes_in_level[0] = u[0];
+    for (size_t i = 0; i < u[0]; i++){
+        levels[0][i].value = i;
+    }
+
+    for (size_t i = 1; i < n; i++){
+        // cerr << "Level " << i << " number of active nodes in father level = " << active_nodes_in_level[i-1] << endl;
+        for (size_t father = 0; father < active_nodes_in_level[i-1]; father++){
+            // cerr << "Father "<< father << " = ";
+            // for (size_t j = 0; j < i; j++) cerr << levels[i-1][father][j] << " ";
+            // cerr << endl;
+            for (size_t j = 0; j < u[i]; j++){
+                auto compatible = true;
+                auto currently_checking = &(levels[i-1][father]);
+                for (int k = i - 1; k >= 0; k--){
+                    // cout << "Checking " << i << " " << k << " " << j << " " << levels[i-1][father][k] << endl;
+                    if ( C[i][k] == 1 && currently_checking->value == j){
+                        compatible = false;
+                        break;
+                    }
+                    currently_checking = currently_checking->head;
+                }
+                if (compatible) {
+                    // memcpy(levels[i][active_nodes_in_level[i]], levels[i-1][father], i*sizeof(int));
+                    levels[i][active_nodes_in_level[i]].head = &levels[i-1][father];
+                    levels[i][active_nodes_in_level[i]].value = j;
+                    active_nodes_in_level[i]++;
+                    // levels[i].push_back(levels[i-1][father]); levels[i][levels[i].size()-1].push_back(j);
+                }
+            }
+        }
+    }
+
+    return active_nodes_in_level[n-1];
+}
--- main.cu
+++ main.cu
@@ -1,23 +1,12 @@
-#include "parser.hpp"
 #include <iostream>
 #include <stack>
 #include <vector>
 #include <chrono>
 
+#include "parser.hpp"
+#include "impls/sequential_impl.hpp"
+
 using namespace std;
-
-struct pii {
-    pii * head;
-    int value;
-};
-
-void print_node(pii * node, size_t n){
-    for (size_t i = 0; i < n; i++){
-        cout << node->value << " ";
-        node = node->head;
-    }
-    cout << endl;
-}
 
 int main(int argc, char *argv[]){
     auto input_file = argv[1];
@@ -35,48 +24,12 @@ int main(int argc, char *argv[]){
 
     auto start = chrono::high_resolution_clock::now();
 
-    size_t * level_sizes = new size_t[n];
-    level_sizes[0] = u[0];
-    for (size_t i = 1; i < n; i++) level_sizes[i] = u[i] * level_sizes[i-1];
-
-    size_t * active_nodes_in_level = new size_t[n];
-    memset(active_nodes_in_level, 0, n*sizeof(size_t));
-
-    pii ** levels = new pii*[n];
-    for (size_t i = 0; i < n; i++){
-        levels[i] = new pii[level_sizes[i]];
-    }
-
-    active_nodes_in_level[0] = u[0];
-    for (size_t i = 0; i < u[0]; i++){
-        levels[0][i].value = i;
-    }
-
-    for (size_t i = 1; i < n; i++){
-        for (size_t father = 0; father < active_nodes_in_level[i-1]; father++){
-            for (size_t j = 0; j < u[i]; j++){
-                auto compatible = true;
-                auto currently_checking = &(levels[i-1][father]);
-                for (int k = i - 1; k >= 0; k--){
-                    if ( C[i][k] == 1 && currently_checking->value == j){
-                        compatible = false;
-                        break;
-                    }
-                    currently_checking = currently_checking->head;
-                }
-                if (compatible) {
-                    levels[i][active_nodes_in_level[i]].head = &levels[i-1][father];
-                    levels[i][active_nodes_in_level[i]].value = j;
-                    active_nodes_in_level[i]++;
-                }
-            }
-        }
-    }
+    size_t n_sol_cpu = pne_seq(C, u, n);
 
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> elapsed = end - start;
 
-    cout << "Size of last level = " << active_nodes_in_level[n-1] << endl;
+    cout << "Size of last level = " << n_sol_cpu << endl;
     cout << "Elapsed time: " << elapsed.count() << " ms" << endl;
 
     return 0;
